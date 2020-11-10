c2e0204e5828fe0d05803ee2724c2864fe6f33b3
ilya83065 <ilyathomas8306@icloud.com>
2020-11-10 15:56:27 -0500

    search and pagination

--- karaoke_online_hakaton/application/routes.py
+++ karaoke_online_hakaton/application/routes.py
@@ -1,7 +1,7 @@
 import mongoengine
 from flask import Flask, render_template, Blueprint, url_for, redirect, session, request, send_from_directory, abort
 from flask_security import login_required, roles_required
-from DataBase import Song
+from DataBase import *
 
 general = Blueprint('general', __name__)
 
@@ -9,6 +9,27 @@ general = Blueprint('general', __name__)
 @general.route('/')
 def index():
     return render_template("mainpage.html")
+
+@general.route('/lobby')
+@login_required
+def lobby():
+    q = request.args.get("q")
+
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page=1
+
+    if q:
+        songs = Song.objects(name__contains=q)
+    else:
+        songs = Song.objects
+
+    pages = songs.paginate(page=page, per_page=1)
+
+    return render_template("lobby.html", songs=songs, pages=pages)
 
 
 @general.route('/<song_id>')
